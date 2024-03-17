f02454765e14e1309874c661d37dac1cf3ee2875
timvq68551 <54223159+timvq68551@users.noreply.github.com>
2024-03-17 10:40:35 -0700

    Added progress check script

---
+++ progress_check.py
@@ -0,0 +1,12 @@
+"""
+Add your comment/header block here
+"""
+
+# Use the empty print() statements to respond to the prompts below
+print("The hardest part about learning Python so far is...")
+print()  # your response here
+print("The most interesting part about learning Python so far is...")
+print()
+print("One thing related to Python I'd like to know more about is...")
+print()
+print("One thing I'd tell future students about this course is...")
